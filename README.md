eaa647e52d534da46f49d316ba78ed5a8ddf8268
Oreste Luci <oreste@luci.cl>
2015-09-06 17:07:10 +0200

    Updating maps.groovy

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Run groovy scripts with:
     $ groovy <your-script>.groovy
 
 
+## Groovy JDK API Documentation
+
+[http://docs.groovy-lang.org/latest/html/groovy-jdk/index-all.html](http://docs.groovy-lang.org/latest/html/groovy-jdk/index-all.html)
+
+Groovy web [site](http://www.groovy-lang.org/index.html)
--- collections/maps.groovy
+++ collections/maps.groovy
@@ -12,3 +12,26 @@ def aMap = [
 // After this initialization you will get a new LinkedHashMap with the entries: Key1 -> Value1, Key2 -> Value 2, Key3 -> Another Value. If you want to create an empty map with this syntax you can write: def map = [:]
 println aMap['Key1'] // Access the key Key1
 println aMap[key] // Access the entry with the value of key variable
+
+// Beside using brackets you can also use dot notation to access entries:
+println aMap.Key1 // Access Key1 entry
+
+// If the key has characters in it, that can’t be parsed as an identifier (e.g. a space) you can still use the dot syntax with quotes around it: aMap.'Key With Spaces'. Since in my opinion this looks kind of strange I don’t give it its own box. You can even use interpolated Strings with this syntax, like: aMap."$key", which is the same as aMap[key].
+
+// Transforming and Filtering
+println "\nTransforming and Filtering"
+aMap.each {
+    print it.key
+    print ", "
+    print it.value
+    println " "
+}
+
+//Instead of the value you will get the entry of the map on which you can access the key and value as shown above. The each method can also take a closure with two parameters in which case it will put key and value into these parameters. So you could express  the above also as:
+println " "
+aMap.each { k, v ->
+    print k
+    print ", "
+    print v
+    println " "
+}
